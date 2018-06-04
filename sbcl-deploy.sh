085d30ece93fa7ddcc3ac5d5458e1bb25d8716c3
Eugene Chupriyanov <tchu@tchu.ru>
2018-06-04 19:28:39 +0300

    Correct naming for folders/arch/platform

--- sbcl-deploy.sh
+++ sbcl-deploy.sh
@@ -4,11 +4,14 @@ version=${VERSION:-1.4.7}
 prefix=${PREFIX:-/tmp/sbcl/sbcl-bin}
 project_dir="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-if [ -z ${arch+x} ]; then
-  echo "\$arch is unset."
-  echo "Expected architecture (e.g. x86-64-linux)"
+if [ -z ${platform+x} ]; then
+  echo "\$platform is unset."
+  echo "Expected architecture (e.g. linux)"
   exit 1
 fi
+
+workdir=work/${platform}
+arch=x86-64-${platform}
 
 cd $workdir
 rm -rf sbcl-${version}-${arch}
